c35a92917ccbd995a3dc5429d670df90e05fb738
Lenora Rigoni <lenorarigoni@gmail.com>
2017-08-30 14:35:16 -0400

    attempt at auth

--- app.js
+++ app.js
@@ -4,8 +4,32 @@ const path = require('path')
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
+//authorization
+require('dotenv').config();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const session = require('express-session');
+app.use(session({
+  secret: process.env.SESSION_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+const authRouter = require('./routes/auth-routes');
+app.use('/auth', authRouter);
+
+const authHelpers = require('./services/auth/auth-helpers');
+app.use(authHelpers.loginRequired)
+
+
+//everything else in app
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
