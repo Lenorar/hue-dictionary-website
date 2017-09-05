83b8830b2c31fadf962785208b6471e142ad99c9
Lenora Rigoni <lenorarigoni@gmail.com>
2017-09-05 11:58:02 -0400

    fixed username display on profile page

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -9,7 +9,7 @@ usersController.listOfWords = (req, res, next) => {
     .then( words => {
       console.log(words)
       res.render('user/profile', {
-       words: words
+       word: words
      });
     })
     .catch(err => {
